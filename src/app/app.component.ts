2b8683d408c70daadb29379a96a9e370a702f01e
boriskiva <boriskiva3@gmail.com>
2024-05-05 10:20:59 +0300

    moving some component to SCREEN folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {ScreenComponent} from "./components/screen/screen.component";
+import {ScreenComponent} from "./components/SCREEN/screen/screen.component";
 
 @Component({
   selector: 'app-root',
@@ -17,13 +17,17 @@ export class AppComponent implements OnInit {
   }
 
   CalculateSizes() {
-    var height = Math.min(768, Math.max(480, window.innerHeight - 128));
-    var width = Math.min(1024, Math.max(640, window.innerWidth - 128));
+    var height = Math.min(768, Math.max(480, window.innerHeight - 168));
+    var width = Math.min(1024, Math.max(640, window.innerWidth - 168));
 
     if (width * 3 < height * 4) height = width / 4 * 3;
     else width = height / 3 * 4;
 
+    let top_bar_height = 25 * height / 768;
+    console.log(top_bar_height);
+
     document.documentElement.style.setProperty('--screen-width', `${width}px`);
     document.documentElement.style.setProperty('--screen-height', `${height}px`);
+    document.documentElement.style.setProperty('--top-bar-height', `${top_bar_height}px`);
   }
 }
--- src/app/components/SCREEN/screen/screen.component.ts
+++ src/app/components/SCREEN/screen/screen.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {TopBarComponent} from "../top-bar/top-bar.component";
+import {ScreenContentComponent} from "../screen-content/screen-content.component";
 
 @Component({
   selector: 'app-screen',
   standalone: true,
-  imports: [],
+  imports: [
+    TopBarComponent,
+    ScreenContentComponent
+  ],
   templateUrl: './screen.component.html',
   styleUrl: './screen.component.css'
 })

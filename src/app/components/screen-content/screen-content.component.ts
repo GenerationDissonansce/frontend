7a009d4f17e9c1f814f956dd38aba19731c57880
boriskiva <boriskiva3@gmail.com>
2024-05-05 10:01:37 +0300

    creating screen content component

---
+++ src/app/components/screen-content/screen-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-screen-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './screen-content.component.html',
+  styleUrl: './screen-content.component.css'
+})
+export class ScreenContentComponent {
+
+}

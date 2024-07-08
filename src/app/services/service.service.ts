53bf88c3dd9846721aa1f83219ca98988103e393
boriskiva <boriskiva3@gmail.com>
2024-07-08 14:10:33 +0300

    fixing mistakes

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -7,8 +7,6 @@ import {ProductModel} from "../models/product.model";
 export class ServiceService {
   public products: ProductModel[] = [
     {src: 'assets/products/t-shirt-1.jpg', name:'WHITE COIN TEE', price: 6000, images: ['assets/products/t-shirt-1.jpg', 'assets/products/t-shirt-2.jpg', 'assets/products/t-shirt-3.jpg'], valid: true},
-    {src: 'assets/products/coming-soon.png', name:'', price: 0, images: ['assets/products/coming-soon.png'], valid: false},
-    {src: 'assets/products/coming-soon.png', name:'', price: 0, images: ['assets/products/coming-soon.png'], valid: false},
   ];
 
   constructor() {
